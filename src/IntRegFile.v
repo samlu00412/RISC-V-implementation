7e408982cfef9ec9618956310280a7b165ab0733
samlu00412 <79298245+samlu00412@users.noreply.github.com>
2026-08-14 02:19:04 +0800

    feat: build integer register file

--- src/IntRegFile.v
+++ src/IntRegFile.v
@@ -13,4 +13,25 @@ module IntRegFile (
     output [31:0] read_data2
 );
     
+    reg [31:0] registers [31:0];
+    integer i;
+
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            for (i = 0; i < 32; i = i+1) begin
+                registers[i] <= 32'h00000000;
+            end
+        end
+        else begin
+            if(write_enable && (write_reg != 5'd0))
+                registers[write_reg] <= write_data;
+        end
+    end
+
+    assign read_data1 = (read_reg1 == 5'd0) ? 32'd0 : 
+    ((write_enable && (write_reg == read_reg1)) ? write_data : registers[read_reg1]);
+
+    assign read_data2 = (read_reg2 == 5'd0) ? 32'd0 :
+    ((write_enable && (write_reg == read_reg2)) ? write_data : registers[read_reg2]);
+    
 endmodule
